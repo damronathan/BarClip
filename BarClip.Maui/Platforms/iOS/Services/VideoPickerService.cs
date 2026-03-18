4a485c16c39300f697ea9ebfad914afd6d5389e5
damronathan <damronnathan14@gmail.com>
2026-03-18 18:18:02 -0500

    picker update

--- BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
+++ BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
@@ -1,4 +1,4 @@
-﻿using MobileCoreServices;
+﻿using Foundation;
 using Photos;
 using PhotosUI;
 
@@ -45,25 +45,52 @@ public class VideoPickerService
             var fileResults = new List<FileResult>();
             var remaining = results.Length;
 
+            SentrySdk.AddBreadcrumb($"Starting copy of {results.Length} videos");
+
             foreach (var result in results)
             {
-                result.ItemProvider.LoadFileRepresentation(
-                "public.movie",
-                (url, error) =>
+                var assetIdentifier = result.AssetIdentifier;
+                var fetchResult = PHAsset.FetchAssetsUsingLocalIdentifiers(new[] { assetIdentifier }, null); var asset = fetchResult.firstObject as PHAsset;
+
+                if (asset == null)
                 {
-                    if (error != null)
-                        SentrySdk.AddBreadcrumb($"LoadFileRepresentation error: {error.LocalizedDescription}");
-
-                    if (url?.Path != null)
-                    {
-                        var tempPath = Path.Combine(Path.GetTempPath(), Path.GetFileName(url.Path));
-                        File.Copy(url.Path, tempPath, true);
-                        fileResults.Add(new FileResult(tempPath));
-                    }
-
+                    SentrySdk.AddBreadcrumb($"Asset not found for identifier: {assetIdentifier}");
                     if (Interlocked.Decrement(ref remaining) == 0)
                         _tcs.SetResult(fileResults);
-                });
+                    continue;
+                }
+
+                var resources = PHAssetResource.GetAssetResources(asset);
+                var videoResource = resources.FirstOrDefault(r => r.ResourceType == PHAssetResourceType.Video);
+
+                if (videoResource == null)
+                {
+                    SentrySdk.AddBreadcrumb($"No video resource found for asset: {assetIdentifier}");
+                    if (Interlocked.Decrement(ref remaining) == 0)
+                        _tcs.SetResult(fileResults);
+                    continue;
+                }
+
+                var tempUrl = NSUrl.FromFilename(Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".MOV"));
+                var options = new PHAssetResourceRequestOptions { NetworkAccessAllowed = true };
+
+                PHAssetResourceManager.DefaultManager.WriteData(
+    videoResource,
+    tempUrl,
+    options,
+    (error) =>
+    {
+        if (error != null)
+            SentrySdk.AddBreadcrumb($"WriteData error: {error.LocalizedDescription}");
+        else
+        {
+            SentrySdk.AddBreadcrumb($"WriteData complete: {tempUrl.Path}");
+            fileResults.Add(new FileResult(tempUrl.Path));
+        }
+
+        if (Interlocked.Decrement(ref remaining) == 0)
+            _tcs.SetResult(fileResults);
+    });
             }
         }
     }
