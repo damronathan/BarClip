b77c5f8cf6187831c299636d972cbd242f26f1e9
damronathan <damronnathan14@gmail.com>
2026-03-20 18:25:51 -0500

    Selection order change

--- BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
+++ BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
@@ -42,55 +42,54 @@ public class VideoPickerService
                 return;
             }
 
-            var fileResults = new List<FileResult>();
+            var fileResults = new FileResult[results.Length];
+
+
             var remaining = results.Length;
 
             SentrySdk.AddBreadcrumb($"Starting copy of {results.Length} videos");
 
-            foreach (var result in results)
+            for (int i = 0; i < results.Length; i++)
             {
+                var index = i;
+                var result = results[i];
                 var assetIdentifier = result.AssetIdentifier;
-                var fetchResult = PHAsset.FetchAssetsUsingLocalIdentifiers(new[] { assetIdentifier }, null); var asset = fetchResult.firstObject as PHAsset;
-
+                var fetchResult = PHAsset.FetchAssetsUsingLocalIdentifiers(new[] { assetIdentifier }, null);
+                var asset = fetchResult.firstObject as PHAsset;
                 if (asset == null)
                 {
                     SentrySdk.AddBreadcrumb($"Asset not found for identifier: {assetIdentifier}");
                     if (Interlocked.Decrement(ref remaining) == 0)
-                        _tcs.SetResult(fileResults);
+                        _tcs.SetResult(fileResults.Where(f => f != null).ToList());
                     continue;
                 }
-
                 var resources = PHAssetResource.GetAssetResources(asset);
                 var videoResource = resources.FirstOrDefault(r => r.ResourceType == PHAssetResourceType.Video);
-
                 if (videoResource == null)
                 {
                     SentrySdk.AddBreadcrumb($"No video resource found for asset: {assetIdentifier}");
                     if (Interlocked.Decrement(ref remaining) == 0)
-                        _tcs.SetResult(fileResults);
+                        _tcs.SetResult(fileResults.Where(f => f != null).ToList());
                     continue;
                 }
-
                 var tempUrl = NSUrl.FromFilename(Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".MOV"));
                 var options = new PHAssetResourceRequestOptions { NetworkAccessAllowed = true };
-
                 PHAssetResourceManager.DefaultManager.WriteData(
-    videoResource,
-    tempUrl,
-    options,
-    (error) =>
-    {
-        if (error != null)
-            SentrySdk.AddBreadcrumb($"WriteData error: {error.LocalizedDescription}");
-        else
-        {
-            SentrySdk.AddBreadcrumb($"WriteData complete: {tempUrl.Path}");
-            fileResults.Add(new FileResult(tempUrl.Path));
-        }
-
-        if (Interlocked.Decrement(ref remaining) == 0)
-            _tcs.SetResult(fileResults);
-    });
+                    videoResource,
+                    tempUrl,
+                    options,
+                    (error) =>
+                    {
+                        if (error != null)
+                            SentrySdk.AddBreadcrumb($"WriteData error: {error.LocalizedDescription}");
+                        else
+                        {
+                            SentrySdk.AddBreadcrumb($"WriteData complete: {tempUrl.Path}");
+                            fileResults[index] = new FileResult(tempUrl.Path);
+                        }
+                        if (Interlocked.Decrement(ref remaining) == 0)
+                            _tcs.SetResult(fileResults.Where(f => f != null).ToList());
+                    });
             }
         }
     }
