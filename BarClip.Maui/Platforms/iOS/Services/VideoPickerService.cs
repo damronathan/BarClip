ed4c89c328cdd4d9dcf48e9ab68040edae2efd6b
damronathan <damronnathan14@gmail.com>
2026-03-28 15:41:56 -0500

    Picker changes

--- BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
+++ BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
@@ -7,15 +7,19 @@ public class VideoPickerService
     public async Task<List<FileResult>> PickVideosAsync()
     {
         var tcs = new TaskCompletionSource<List<FileResult>>();
+
         var config = new PHPickerConfiguration(PHPhotoLibrary.SharedPhotoLibrary)
         {
             Filter = PHPickerFilter.VideosFilter,
             SelectionLimit = 0
         };
+
         var picker = new PHPickerViewController(config);
         picker.Delegate = new PickerDelegate(tcs);
+
         var vc = Platform.GetCurrentUIViewController();
         vc?.PresentViewController(picker, true, null);
+
         return await tcs.Task;
     }
 
@@ -25,7 +29,7 @@ public class VideoPickerService
 
         public PickerDelegate(TaskCompletionSource<List<FileResult>> tcs) => _tcs = tcs;
 
-        public override void DidFinishPicking(PHPickerViewController picker, PHPickerResult[] results)
+        public override async void DidFinishPicking(PHPickerViewController picker, PHPickerResult[] results)
         {
             picker.DismissViewController(true, null);
 
@@ -35,7 +39,6 @@ public class VideoPickerService
                 return;
             }
 
-            // Fetch and sort assets by creation date
             var identifiers = results.Select(r => r.AssetIdentifier).ToArray();
             var fetchResult = PHAsset.FetchAssetsUsingLocalIdentifiers(identifiers, null);
             var assets = Enumerable.Range(0, (int)fetchResult.Count)
@@ -44,10 +47,9 @@ public class VideoPickerService
                 .OrderBy(a => (DateTime)a!.CreationDate)
                 .ToList();
 
-            var fileResults = new List<FileResult>();
-            var remaining = assets.Count;
-
             SentrySdk.AddBreadcrumb($"Starting copy of {assets.Count} videos");
+
+            var fileResults = new List<FileResult>();
 
             foreach (var asset in assets)
             {
@@ -57,33 +59,27 @@ public class VideoPickerService
                 if (videoResource == null)
                 {
                     SentrySdk.AddBreadcrumb($"No video resource found for asset: {asset!.LocalIdentifier}");
-                    if (Interlocked.Decrement(ref remaining) == 0)
-                        _tcs.SetResult(fileResults);
                     continue;
                 }
 
-                var tempUrl = NSUrl.FromFilename(Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".MOV"));
+                var tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".MOV");
+                var tempUrl = NSUrl.FromFilename(tempPath);
                 var options = new PHAssetResourceRequestOptions { NetworkAccessAllowed = true };
 
-                PHAssetResourceManager.DefaultManager.WriteData(
-                    videoResource,
-                    tempUrl,
-                    options,
-                    (error) =>
-                    {
-                        if (error != null)
-                            SentrySdk.AddBreadcrumb($"WriteData error: {error.LocalizedDescription}");
-                        else
-                        {
-                            SentrySdk.AddBreadcrumb($"WriteData complete: {tempUrl.Path}");
-                            lock (fileResults)
-                                fileResults.Add(new FileResult(tempUrl.Path));
-                        }
-
-                        if (Interlocked.Decrement(ref remaining) == 0)
-                            _tcs.SetResult(fileResults);
-                    });
+                try
+                {
+                    await PHAssetResourceManager.DefaultManager.WriteDataAsync(videoResource, tempUrl, options);
+                    SentrySdk.AddBreadcrumb($"WriteData complete: {tempPath}");
+                    fileResults.Add(new FileResult(tempPath));
+                }
+                catch (Exception ex)
+                {
+                    SentrySdk.CaptureException(ex);
+                    SentrySdk.AddBreadcrumb($"WriteData failed for asset {asset!.LocalIdentifier}: {ex.Message}");
+                }
             }
+
+            _tcs.SetResult(fileResults);
         }
     }
 }
