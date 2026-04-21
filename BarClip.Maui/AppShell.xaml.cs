3198ed329c49c23ceb9e93b2a9a346af149f79d5
damronathan <damronnathan14@gmail.com>
2026-04-21 18:21:28 -0500

    last chance

--- BarClip.Maui/AppShell.xaml.cs
+++ BarClip.Maui/AppShell.xaml.cs
@@ -11,7 +11,7 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(SessionLibrary), typeof(SessionLibrary));
         Routing.RegisterRoute(nameof(SessionPage), typeof(SessionPage));
         Routing.RegisterRoute(nameof(Camera), typeof(Camera));
-        Routing.RegisterRoute(nameof(VideoLibrary), typeof(VideoLibrary));
+        Routing.RegisterRoute(nameof(VideoLibrary), typeof(VideoLibrary));      
         Routing.RegisterRoute(nameof(VideoPlayer), typeof(VideoPlayer));
 
     }
