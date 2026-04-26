539557a41176b087afd61bff404b5aeeb648b304
damronathan <damronnathan14@gmail.com>
2026-04-26 15:34:04 -0500

    decoding removal

--- BarClip.Maui/VideoPlayerView.xaml.cs
+++ BarClip.Maui/VideoPlayerView.xaml.cs
@@ -6,12 +6,13 @@ namespace BarClip.Maui;
 public partial class VideoPlayerView : ContentPage
 {
     private string _videoUrl;
+
     public string VideoUrl
     {
         get => _videoUrl;
         set
         {
-            _videoUrl = Uri.UnescapeDataString(value);
+            _videoUrl = value;
             OnPropertyChanged();
         }
     }
@@ -19,9 +20,9 @@ public partial class VideoPlayerView : ContentPage
     public VideoPlayerView()
     {
         InitializeComponent();
-        _videoUrl = "https://www.w3schools.com/html/mov_bbb.mp4";
         BindingContext = this;
     }
+
     private void OnMediaFailed(object sender, MediaFailedEventArgs e)
     {
         System.Diagnostics.Debug.WriteLine($"Media failed: {e.ErrorMessage}");
