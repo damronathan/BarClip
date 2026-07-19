d9f156da9fb06338b5e9f81beb4b23aeff5e532b
damronathan <damronnathan14@gmail.com>
2026-07-19 09:18:20 -0500

    Fix camera permission not re-prompting after initial denial

--- BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
+++ BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
@@ -1,4 +1,5 @@
-﻿using Foundation;
+﻿using AVFoundation;
+using Foundation;
 using Photos;
 using PhotosUI;
 using UIKit;
@@ -26,6 +27,23 @@ public class VideoPickerService
 
     public async Task<FileResult?> CaptureVideoAsync()
     {
+        var authStatus = AVCaptureDevice.GetAuthorizationStatus(AVAuthorizationMediaType.Video);
+
+        if (authStatus == AVAuthorizationStatus.NotDetermined)
+        {
+            var granted = await AVCaptureDevice.RequestAccessForMediaTypeAsync(AVAuthorizationMediaType.Video);
+            if (!granted)
+            {
+                SentrySdk.AddBreadcrumb("Camera access denied on first request");
+                return null;
+            }
+        }
+        else if (authStatus == AVAuthorizationStatus.Denied || authStatus == AVAuthorizationStatus.Restricted)
+        {
+            await ShowCameraPermissionDeniedAlertAsync();
+            return null;
+        }
+
         var tcs = new TaskCompletionSource<FileResult?>();
 
         var picker = new UIImagePickerController
@@ -42,6 +60,32 @@ public class VideoPickerService
         vc?.PresentViewController(picker, true, null);
 
         return await tcs.Task;
+    }
+
+    private async Task ShowCameraPermissionDeniedAlertAsync()
+    {
+        var tcs = new TaskCompletionSource<bool>();
+
+        var alert = UIAlertController.Create(
+            "Camera Access Needed",
+            "BarClip needs camera access to record videos. Please enable it in Settings.",
+            UIAlertControllerStyle.Alert);
+
+        alert.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, _ => tcs.SetResult(false)));
+        alert.AddAction(UIAlertAction.Create("Open Settings", UIAlertActionStyle.Default, _ =>
+        {
+            var settingsUrl = new NSUrl(UIApplication.OpenSettingsUrlString);
+            if (UIApplication.SharedApplication.CanOpenUrl(settingsUrl))
+            {
+                UIApplication.SharedApplication.OpenUrl(settingsUrl, new UIApplicationOpenUrlOptions(), null);
+            }
+            tcs.SetResult(true);
+        }));
+
+        var vc = Platform.GetCurrentUIViewController();
+        vc?.PresentViewController(alert, true, null);
+
+        await tcs.Task;
     }
 
     private class CameraDelegate : UIImagePickerControllerDelegate
