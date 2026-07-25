daef8a8a0c224f410cb9a462d7e7b40ef1a49f9b
damronathan <damronnathan14@gmail.com>
2026-07-24 20:47:14 -0500

    add press animation behavior to buttons for visible tap feedback

---
+++ BarClip.Maui/Behaviors/PressAnimationBehavior.cs
@@ -0,0 +1,30 @@
+﻿namespace BarClip.Maui.Behaviors;
+
+public class PressAnimationBehavior : Behavior<Button>
+{
+    protected override void OnAttachedTo(Button button)
+    {
+        button.Pressed += OnPressed;
+        button.Released += OnReleased;
+        base.OnAttachedTo(button);
+    }
+
+    protected override void OnDetachingFrom(Button button)
+    {
+        button.Pressed -= OnPressed;
+        button.Released -= OnReleased;
+        base.OnDetachingFrom(button);
+    }
+
+    private async void OnPressed(object? sender, EventArgs e)
+    {
+        if (sender is Button button)
+            await button.ScaleTo(0.85, 60, Easing.CubicOut);
+    }
+
+    private async void OnReleased(object? sender, EventArgs e)
+    {
+        if (sender is Button button)
+            await button.ScaleTo(1, 60, Easing.CubicOut);
+    }
+}
