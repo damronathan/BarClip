db86a528cb472fc10a707b356b1f1412256a3252
damronathan <damronnathan14@gmail.com>
2026-09-14 10:45:17 -0500

    Comment out old camera

--- BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
+++ BarClip.Maui/Platforms/iOS/Services/VideoPickerService.cs
@@ -25,42 +25,7 @@ public class VideoPickerService
         return await tcs.Task;
     }
 
-    public async Task<FileResult?> CaptureVideoAsync()
-    {
-        var authStatus = AVCaptureDevice.GetAuthorizationStatus(AVAuthorizationMediaType.Video);
 
-        if (authStatus == AVAuthorizationStatus.NotDetermined)
-        {
-            var granted = await AVCaptureDevice.RequestAccessForMediaTypeAsync(AVAuthorizationMediaType.Video);
-            if (!granted)
-            {
-                SentrySdk.AddBreadcrumb("Camera access denied on first request");
-                return null;
-            }
-        }
-        else if (authStatus == AVAuthorizationStatus.Denied || authStatus == AVAuthorizationStatus.Restricted)
-        {
-            await ShowCameraPermissionDeniedAlertAsync();
-            return null;
-        }
-
-        var tcs = new TaskCompletionSource<FileResult?>();
-
-        var picker = new UIImagePickerController
-        {
-            SourceType = UIImagePickerControllerSourceType.Camera,
-            MediaTypes = new string[] { "public.movie" },
-            VideoQuality = 0,
-            VideoMaximumDuration = 90
-        };
-
-        picker.Delegate = new CameraDelegate(tcs);
-
-        var vc = Platform.GetCurrentUIViewController();
-        vc?.PresentViewController(picker, true, null);
-
-        return await tcs.Task;
-    }
     public async Task<FileResult?> CaptureVideoAsync()
     {
         var authStatus = AVCaptureDevice.GetAuthorizationStatus(AVAuthorizationMediaType.Video);
@@ -216,4 +181,43 @@ public class VideoPickerService
             _tcs.SetResult(fileResults);
         }
     }
+
+
+    //public async Task<FileResult?> CaptureVideoAsync()
+    //{
+    //    var authStatus = AVCaptureDevice.GetAuthorizationStatus(AVAuthorizationMediaType.Video);
+
+    //    if (authStatus == AVAuthorizationStatus.NotDetermined)
+    //    {
+    //        var granted = await AVCaptureDevice.RequestAccessForMediaTypeAsync(AVAuthorizationMediaType.Video);
+    //        if (!granted)
+    //        {
+    //            SentrySdk.AddBreadcrumb("Camera access denied on first request");
+    //            return null;
+    //        }
+    //    }
+    //    else if (authStatus == AVAuthorizationStatus.Denied || authStatus == AVAuthorizationStatus.Restricted)
+    //    {
+    //        await ShowCameraPermissionDeniedAlertAsync();
+    //        return null;
+    //    }
+
+    //    var tcs = new TaskCompletionSource<FileResult?>();
+
+    //    var picker = new UIImagePickerController
+    //    {
+    //        SourceType = UIImagePickerControllerSourceType.Camera,
+    //        MediaTypes = new string[] { "public.movie" },
+    //        VideoQuality = 0,
+    //        VideoMaximumDuration = 90
+    //    };
+
+    //    picker.Delegate = new CameraDelegate(tcs);
+
+    //    var vc = Platform.GetCurrentUIViewController();
+    //    vc?.PresentViewController(picker, true, null);
+
+    //    return await tcs.Task;
+    //}
+
 }
